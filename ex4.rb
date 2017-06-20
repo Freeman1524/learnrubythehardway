64b19f55bd086102b1b690966da786cd33583d52
zachfreeman <freeman1524@outlook.com>
2017-06-20 15:11:07 -0400

    Added comments to explain

--- ex4.rb
+++ ex4.rb
@@ -1,16 +1,21 @@
-cars = 100
-space_in_a_car = 4.0
-drivers = 30
-passengers = 90
-cars_not_driven = cars - drivers
-cars_driven = drivers
-carpool_capacity = cars_driven * space_in_a_car
-average_passengers_per_car = passengers / cars_driven
+cars = 100 # amount of cars
+space_in_a_car = 4.0 # how many people car can hold
+drivers = 30 # amount of drivers
+passengers = 90 # amount of passengers
+cars_not_driven = cars - drivers # cars not being driven
+cars_driven = drivers # cars being driven
+carpool_capacity = cars_driven * space_in_a_car # how many people can be transported
+average_passengers_per_car = passengers / cars_driven #average passenger per car
 
-#
+# Shows cars available
 puts "There are #{cars} cars available."
+# Shows drivers available
 puts "There are only #{drivers} drivers available."
+# Shows empty cars
 puts "There will be #{cars_not_driven} empty cars today."
+# Shows amounty of people that can be transported
 puts "We can transport #{carpool_capacity} people today."
+# Shows amounter of passengers being carpooled today.
 puts "We have #{passengers} to carpool today."
+# Shows the average amount of passengers we can hold per car.
 puts "We need to put about #{average_passengers_per_car} in each car."
