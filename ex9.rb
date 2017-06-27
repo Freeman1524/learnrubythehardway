7abbd3cb22285057031dcb6a560a3f826eff646e
zachfreeman <freeman1524@outlook.com>
2017-06-27 11:53:32 -0400

    Explained code

---
+++ ex9.rb
@@ -0,0 +1,19 @@
+# Her'es some new strange stuff, remember type it exactly.
+
+# creates variable days and sets it to the sting below
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# creates variable for months and sets it to the string below also uses \nto make a new line
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# prints here are the days an uses # operator to show program youre going to pull the variable.
+puts "Here are the days: #{days}"
+# prints string below and uses # operator to show months variable  
+puts "Here are the months: #{months}"
+
+# Uses %q to make a single quote string
+puts %q{
+	Theres something going on here.
+	With this weird quote
+	We'll be able to type as much as we like.
+	Even 4 lines if we want, or 5, or 6.
+}
