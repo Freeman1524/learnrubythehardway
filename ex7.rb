0e234fc9d0ea1e13e92c6991433cec9f9fa0a6f3
zachfreeman <freeman1524@outlook.com>
2017-06-27 09:15:20 -0400

    Explained what the code does

--- ex7.rb
+++ ex7.rb
@@ -1,8 +1,12 @@
+# prints string to console
 puts "Mary had a little lamb."
+# prints string to console and uses #  operator to add variable snow
 puts "Its fleece was white as #{'snow'}"
+# prints string to console
 puts "And everywhere that mary went."
-puts "."  * 10 # what'd that do?
+puts "."  * 10 # what'd that do? Printed 10 "." to the consol
 
+#creates variable and and sets each to a letleter
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -16,6 +20,6 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# watch that print vs. puts on this line what's it do?
+# prints vs puts changes whether nor not it starts a new line.
 print end1 + end2 + end3 + end4 + end5 + end6
 puts end7 + end8 + end9 + end10 + end11 + end12
