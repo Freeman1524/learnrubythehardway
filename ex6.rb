1d61064a12713a8dd9f8de5bd42c94e217c0ba00
zachfreeman <freeman1524@outlook.com>
2017-06-26 22:46:56 -0400

    changes to explaination

--- ex6.rb
+++ ex6.rb
@@ -1,12 +1,12 @@
 # creates variable for types_of_peoples and sets it to 10
 types_of_people = 10  
-# creates variable for x and sets it to the string below
+# creates variable for x and sets it to the string below. The "#" tells the program you're gonna be pulling data from the variable.
 x = "There are #{types_of_people} types of people."
-# creates variable for binary and sets it the string binary
+# creates variable for binary and sets it to the string binary
 binary = "binary"
 # creates variable for do_not and sets it to the string "don't"
 do_not = "don't"
-# creates variable for y and sets it to the string below.
+# creates variable for y and sets it to the string below. Uses the # operator to tell program it will be pulling from variable.
 y = "Those who know #{binary} and those who #{do_not}."
 
 # Prints the variable x to the console
@@ -14,12 +14,12 @@ puts x
 # Prints the variable y to the console
 puts y
 
-# Prints the string below with the variable x.
+# Prints the string below with the variable x. the # operator pulls data from the variable
 puts "I said: #{x}." 
-# Prints the string below with teh variable y.
+# Prints the string below with the variable y. the # operator pulls data from the variable.
 puts "I also said: '#{y}'."
 
-#sets variable hilarious to false.
+#creates variable for hilarious and sets it to false.
 hilarious = false
 #sets variable joke evaulation to the string below
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
@@ -28,7 +28,7 @@ joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 puts joke_evaluation
 
 #sets variable for w with the string below.
-w = "This is the left side of..."
+w = 'This is the left side of...'
 #sets variable for e with the string below.
 e = "a string with a right side."
 
