4c6f44fb68dbab885e4c776e4c5a555409756f7d
zachfreeman <freeman1524@outlook.com>
2017-06-26 21:58:03 -0400

    Added explainations

---
+++ ex6.rb
@@ -0,0 +1,36 @@
+# creates variable for types_of_peoples and sets it to 10
+types_of_people = 10  
+# creates variable for x and sets it to the string below
+x = "There are #{types_of_people} types of people."
+# creates variable for binary and sets it the string binary
+binary = "binary"
+# creates variable for do_not and sets it to the string "don't"
+do_not = "don't"
+# creates variable for y and sets it to the string below.
+y = "Those who know #{binary} and those who #{do_not}."
+
+# Prints the variable x to the console
+puts x
+# Prints the variable y to the console
+puts y
+
+# Prints the string below with the variable x.
+puts "I said: #{x}." 
+# Prints the string below with teh variable y.
+puts "I also said: '#{y}'."
+
+#sets variable hilarious to false.
+hilarious = false
+#sets variable joke evaulation to the string below
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+#prints the variable for joke_evaulations
+puts joke_evaluation
+
+#sets variable for w with the string below.
+w = "This is the left side of..."
+#sets variable for e with the string below.
+e = "a string with a right side."
+
+# Prints the variables w + e to the console.
+puts w + e
